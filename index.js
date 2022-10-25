bf7e22ab23abec40fc96103875bdbd0b1deaa6a2
Man Rn <48957574+mariusman12@users.noreply.github.com>
2022-10-25 21:46:45 +0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,30 @@
+const express = require("express");
+const cors = require("cors");
+const morgan = require("morgan");
+const bodyParser = require("body-parser");
+var app = express();
+const port = 3000;
+
+app.use(cors());
+
+app.use(morgan("dev"));
+app.get("/", (req, res) => {
+  res.send("Hello World / !");
+});
+app.use(bodyParser.json({ limit: "50mb" }));
+app.get("/add", (req, res) => {
+  res.send("Hello World add!");
+});
+
+app.get("/add2", (req, res) => {
+  res.send("Hello World add2!");
+});
+app.get("/getData", (req, res) => {
+  fetch("https://reqres.in/api/users")
+    .then((res) => res.json())
+    .then((data) => console.log(data));
+});
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+});
+module.exports = app;
